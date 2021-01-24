a8b9152cb631fed1fa92c7f5f5abbdf4f8eacb27
Dusan Knezevic <dusan.knezevic@fincore.com>
2021-01-24 17:26:08 +0100

    Add project files.

---
+++ VivifyIdeasTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VivifyIdeasTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
